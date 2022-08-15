2c2ee82bb8d674a5032a8c5b2e95641497a694a4
Dmitrijs <dmitrijs.sinickis@gmail.com>
2022-08-15 22:55:09 +0300

    second commit

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 const taskInput = document.getElementById("task");
 const addBtn = document.getElementById("add-task-btn");
-const todosWrapper = document.querySelector("todos-wrapper");
+const todosWrapper = document.querySelector(".todos-wrapper");
 
 let tasks;
 !localStorage.tasks
   ? (tasks = [])
-  : (tasks = JSON.parse(localStorage.getItem(tasks)));
+  : (tasks = JSON.parse(localStorage.getItem("tasks")));
 
 function Task(description) {
   this.description = description;
   this.completed = false;
 }
 
-const updateStorage = () => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-addBtn.addEventListener("click", () => {
-  tasks.push(new Task(taskInput.value));
-  updateStorage();
-});
-
 const createTemplate = (task, index) => {
   return `
-    
-
+  <br><div class="todo-item ${task.completed ? 'checked' : ''}">
+      <div class="description">${task.description}</div>
+      <div class="buttons">
+          <input class="btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}>
+          <div class="btn-remove">Delete</div>
+      </div>
+  </div>
     `;
-};
+}
 
 const fillList = () => {
   todosWrapper.innerHTML = "";
@@ -36,3 +32,15 @@ const fillList = () => {
     });
   }
 };
+
+fillList();
+
+const updateStorage = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+addBtn.addEventListener("click", () => {
+  tasks.push(new Task(taskInput.value));
+  updateStorage();
+  fillList();
+});
